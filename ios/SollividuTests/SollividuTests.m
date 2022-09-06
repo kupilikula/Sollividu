75c9c590d7c4b7059bb7268b42b420467f6d18b8
Anand Murugan <anand.murugan@gmail.com>
2022-09-06 17:40:18 -0500

    rename of app to Sollividu

--- ios/SollividuTests/SollividuTests.m
+++ ios/SollividuTests/SollividuTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MozhiTests : XCTestCase
+@interface SollividuTests : XCTestCase
 
 @end
 
-@implementation MozhiTests
+@implementation SollividuTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
